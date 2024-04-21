2ec552d7bde6b86fbd0eef1c8ecaf6344bc3a6cd
xJellybeanx <167363773+xJellybeanx@users.noreply.github.com>
2024-04-21 08:35:04 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ for (let x = 0; x < topics.length; x++) {
  console.log(topics[x]);
 }
 
-/*
+
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -16,4 +16,3 @@ if (topic === 'HTML') {
 } else {
  console.log('Please try again!');
 }
-*/
